88529ff50d3d0b7522f12bf4abf2f2bf5c2e38f8
Amit Dewal <daredewal007@gmail.com>
2025-10-16 18:36:33 +0530

    math package in python and its functions

---
+++ math/math_operation.py
@@ -0,0 +1,32 @@
+import  math
+print("Square roo of 16: ", math.sqrt(16))
+print("Two raised to the powe of 5: ", math.pow(2,5))
+
+print("Absolute value of -9: ",math.fabs(-9))
+
+print("Floor of 7.9: ", math.floor(7.9))
+print("ceil of 7.1: ", math.ceil(7.1))
+
+print("factorial of 5: ", math.factorial(5))
+
+
+# pi
+print("value of pi :",math.pi)
+print("Euler Number:", math.e)
+print("Pi radians to degree:",math.degrees(math.pi))
+print("Pi radians to degree:",math.degrees(math.pi/2))
+print("degree to radian:",math.radians(180))
+
+#sin cos tan
+print("Sin of PI divided by two:", math.sin(math.pi/2)) # 90
+print("cosine of  zero:", math.cos(0)) # 90
+print("tan of zero:", math.tan(0)) # 90
+
+print("Natural log of ten:",math.log(10))
+print("Log base of one thousand:", math.log10(1000))
+
+# gcd
+print("GCD of fifty-four and twenty-four:", math.gcd(54,24))
+
+print("Integer square root of twenty:", math.isqrt(20))
+
